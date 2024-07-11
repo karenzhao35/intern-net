189207b164a596e211ace129ab8d260b2bbefe83
karenzhao35 <karenzhao35@gmail.com>
2024-07-11 00:36:15 -0700

    updated README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Attributions
+https://medium.com/@jonathanmondaut/web-scraping-glassdoor-job-listings-for-data-analysis-e26939e270f2
+https://scrapfly.io/blog/how-to-scrape-indeedcom/
--- scrape/views.py
+++ scrape/views.py
@@ -2,33 +2,46 @@ from django.http import HttpResponse
 # Create your views here.
 import re
 import json
+import requests
+import os
 from scrapfly import ScrapflyClient, ScrapeConfig
+
+# indeed 
 
 scrapfly = ScrapflyClient(key="scp-live-764641ae102e46e0a74641837f839c45")
 
-result = scrapfly.scrape(
-    ScrapeConfig(
-        url="https://www.indeed.ca/jobs?q=software+intern&l=Canada",
-        asp=True,
-    )
-)
+# result = scrapfly.scrape(
+#     ScrapeConfig(
+#         url="https://www.indeed.ca/jobs?q=software+intern&l=Canada",
+#         asp=True,
+#     )
+# )
 
 
-def parse_search_page(request):
-    data = re.findall(r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});', result.content)
-    if not data:
-        return None
-    data = json.loads(data[0])
+# def parse_search_page(request):
+#     data = re.findall(r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});', result.content)
+#     if not data:
+#         return None
+#     data = json.loads(data[0])
     
-    results = data["metaData"]["mosaicProviderJobCardsModel"]["results"]
-    results = results[:5]
+#     results = data["metaData"]["mosaicProviderJobCardsModel"]["results"]
+#     results = results[:5]
 
-    return HttpResponse(json.dumps({
-        "results": results,
-        "meta": data["metaData"]["mosaicProviderJobCardsModel"]["tierSummaries"],
-    }), content_type="application/json")
+#     return HttpResponse(json.dumps({
+#         "results": results,
+#         "meta": data["metaData"]["mosaicProviderJobCardsModel"]["tierSummaries"],
+#     }), content_type="application/json")
 
+# GLASSDOOR
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 
-# # Example: limit to 5 items
-# print(parse_search_page())
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.glassdoor.ca")
+print(driver.title)
+
+driver.quit()
